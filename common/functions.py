d9f2ba52c3cecd347a2da8e031489baacdf62d96
rapsori <rapsori@gmail.com>
2022-09-23 17:04:01 +0900

    ch04 ing

--- common/functions.py
+++ common/functions.py
@@ -18,3 +18,10 @@ def softmax(a):
 	y = exp_a / sum_exp_a
 	
 	return y
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+t=[0,0,1,0,0,0,0,0,0,0]
+y=[0.1,0.05,0.6,0.0,0.05,0.1,0.0,0.1,0.0,0.0]
+print(mean_squared_error(np.array(y), np.array(t)))
